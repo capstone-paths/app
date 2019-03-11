e91732c1f7b7700e8cbf4031f88b142e72601b3f
alexerhardt <alex.erhardt.es@gmail.com>
2019-03-11 13:29:08 +0100

    add config deployment comment

--- server/config/index.js
+++ server/config/index.js
@@ -1,6 +1,9 @@
 /**
  * config/index.js
  * Sets up ports and MongoDB URIs for local / test modes
+ * TODO: 
+ * setting NODE_ENV is a Heroku thing
+ * check if AWS also works like this
  */
 const env = process.env.NODE_ENV || 'development';
 
