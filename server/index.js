30073253ad5c129976db54275fda639f9730fd79
alexerhardt <alex.erhardt.es@gmail.com>
2019-03-11 18:29:41 +0100

    add /graphs/courses

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 require('./config/index.js');
-require('./config/neo4j');
 
 const path = require('path');
 const express = require('express');
--- server/routes/courses.js
+++ server/routes/courses.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const driver = require('../config/neo4j');
 
 const router = express.Router();
 
@@ -9,7 +10,39 @@ const router = express.Router();
  * This is a temporary route - we probably don't want a public route like this
  */
 router.get('/', (req, res, next) => {
+  const query = `
+  MATCH (c1:Course)<-[:REQUIRED_BY]-(c2:Course)
+  RETURN c1.name AS course, collect(c2.name) AS prereqs
+  `;
 
+  const session = driver.session();
+  session
+    .run(query)
+    .then((results) => {
+      session.close();
+      const [nodes, rels] = [[], []];
+      let i = 0;
+      results.records.forEach((result) => {
+        nodes.push({ course: result.get('course'), label: 'course' });
+        const target = i;
+        i++;
+
+        result.get('prereqs').forEach((prereqName) => {
+          const prereq = { course: prereqName, label: 'course' };
+          let source = nodes.findIndex(elt => elt === prereqName);
+          if (source === -1) {
+            nodes.push(prereq);
+            source = i;
+            i++;
+          }
+          rels.push({ source, target });
+        });
+
+        return res.json({ data: { nodes, rels } });
+      });
+    })
+    .catch(next)
+    .then(() => session.close());
 });
 
 module.exports = router;
