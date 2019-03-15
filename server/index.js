a523e724f321601e510c992f7d4f338ce5e5a7bb
alexerhardt <alex.erhardt.es@gmail.com>
2019-03-15 18:12:52 +0100

    change production port

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ app.use((err, req, res) => {
   res.json({ err });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8081;
 app.listen(port, () => {
   logger.info(`Server listening on port ${port}`);
 });
