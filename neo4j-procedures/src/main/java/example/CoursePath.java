2a3e0b02b438784fc7a0b85ba4c7b8825237aa36
alexerhardt <alex.erhardt.es@gmail.com>
2019-03-21 16:18:11 +0100

    start shouldAddToNextNodes

--- neo4j-procedures/src/main/java/example/CoursePath.java
+++ neo4j-procedures/src/main/java/example/CoursePath.java
@@ -1,12 +1,10 @@
 package example;
 
-import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Relationship;
-import org.neo4j.graphdb.RelationshipType;
+import org.neo4j.graphdb.*;
 import org.neo4j.logging.Log;
 import org.neo4j.procedure.Context;
 import org.neo4j.procedure.*;
+import org.omg.CosNaming.NamingContextPackage.NotFound;
 
 import java.util.*;
 import java.util.stream.Stream;
@@ -21,11 +19,11 @@ public class CoursePath
 
     @Procedure(name = "example.findCoursePath", mode=Mode.SCHEMA)
     public Stream<GraphResult> findCoursePath( @Name("startNode") Object startNode,
-                                               @Name("nodePropName") String nodePropName,
-                                               @Name("nodeLabelName") String nodeLabelName,
-                                               @Name("nodeCategoryName") String nodeCategoryName,
-                                               @Name("linkPropName") String linkPropName,
-                                               @Name("linkLabelName") String linkLabelName,
+                                               @Name("courseWeightPropName") String courseWeightPropName,
+                                               @Name("courseLabelName") String courseLabelName,
+                                               @Name("courseCategoryPropName") String courseCategoryPropName,
+                                               @Name("prereqWeightPropName") String prereqWeightPropName,
+                                               @Name("prereqLabelName") String prereqLabelName,
                                                @Name("threshold") Double threshold)
     {
         if (startNode == null) {
@@ -41,10 +39,11 @@ public class CoursePath
 
         List<Node> nodes = new ArrayList<Node>();
         List<Relationship> relationships = new ArrayList<Relationship>();
-        Set<Node> visited = new HashSet<Node>();
+        Set<Long> visited = new HashSet<>();
 
         findCoursePathPrivate(nodes, relationships, visited, (Node) startNode,
-                              nodePropName, nodeLabelName, linkPropName, linkLabelName, threshold);
+                              courseWeightPropName, courseLabelName, courseCategoryPropName,
+                              prereqWeightPropName, prereqLabelName, threshold);
 
         return Stream.of(new GraphResult(nodes, relationships));
     }
@@ -52,20 +51,27 @@ public class CoursePath
     /**
      *
      */
-    private void findCoursePathPrivate(List<Node> nodes, List<Relationship> rels, Set<Node> visited, Node curNode,
-                                       String nodePropName, String nodeLabelName, String linkPropName,
-                                       String linkLabelName, Double threshold)
+    private void findCoursePathPrivate(List<Node> nodes, List<Relationship> rels, Set<Long> visited, Node curNode,
+                                       String courseWeightPropName, String courseLabelName, String courseCategoryPropName,
+                                       String prereqWeightPropName, String prereqLabelName,
+                                       Double threshold)
     {
         nodes.add(curNode);
 
-        Iterator<Relationship> relsIt = curNode.getRelationships(RelationshipType.withName(linkPropName)).iterator();
+        Iterator<Relationship> relsIt = curNode.getRelationships(RelationshipType.withName(prereqLabelName)).iterator();
 
         if (!relsIt.hasNext()) {
             return;
         }
 
+
         // get node id
         // create array of nodes
+        long id = curNode.getId();
+        HashMap<String, NodeWithWeight> nextNodes = new HashMap<>();
+        Relationship rel = null;
+        Node candidate = null;
+        PrereqInfo prereqInfo = null;
 
         while (relsIt.hasNext()) {
             // get relationship
@@ -74,9 +80,31 @@ public class CoursePath
             // if relationship passes the threshold && (category doesn't exist || is category max)
                 // add relationship to results
                 // add node to nodes to be processed
+            rel = relsIt.next();
+            candidate = rel.getOtherNode(curNode);
+            if (visited.contains(candidate.getId())) {
+                continue;
+            }
+
+            visited.add(candidate.getId());
+            prereqInfo = prereqInfoFromGraph(candidate, courseCategoryPropName, courseWeightPropName, rel, prereqWeightPropName);
+            if (prereqInfo == null) {
+                continue;
+            }
+
+            if (shouldAddToNextNodes(nextNodes, prereqInfo, threshold)) {
+
+            }
+
+
         }
 
         // process next nodes;
+    }
+
+    private boolean shouldAddToNextNodes(HashMap<String, Node> nextNodes, PrereqInfo info, Double threshold)
+    {
+
 
     }
 
@@ -96,13 +124,73 @@ public class CoursePath
         }
     }
 
+    // TODO: Move PrereqInfo into own class and make this static
+    public PrereqInfo prereqInfoFromGraph(Node node, String courseCategoryPropName, String courseWeightPropName,
+                                          Relationship rel, String prereqWeightPropName)
+    {
+        try {
+            Object nodeCategory = node.getProperty(courseCategoryPropName);
+            if (!(nodeCategory instanceof String)) {
+                log.debug("Node course category property is not an String but a: " + nodeCategory.getClass());
+                return null;
+            }
 
-    private class NodeWithGraphWeight
+            Object nodeRecommendations = node.getProperty(courseWeightPropName);
+            if (!(nodeRecommendations instanceof Integer)) {
+                log.debug("Node recommendations property is not an Integer but a: " + nodeCategory.getClass());
+                return null;
+            }
+
+            Object relRecommendations = rel.getProperty(prereqWeightPropName);
+            if (!(relRecommendations instanceof Integer)) {
+                log.debug("Relationship recommendations property is not an Integer but a: "
+                            + prereqWeightPropName.getClass());
+                return null;
+            }
+
+            return new PrereqInfo((String) nodeCategory, (Integer) relRecommendations, (Integer) nodeRecommendations);
+        }
+        catch (NotFoundException e) {
+            log.debug("Graph object property not found: " + e.getMessage());
+            return null;
+        }
+    }
+
+    private class PrereqInfo
+    {
+        private String category;
+        private Integer numberOfPreReqs;
+        private Integer numberOfCourseRecommendations;
+
+        public PrereqInfo(String category, Integer numberOfPreReqs, Integer numberOfCourseRecommendations) {
+            this.category = category;
+            this.numberOfPreReqs = numberOfPreReqs;
+            this.numberOfCourseRecommendations = numberOfCourseRecommendations;
+        }
+
+        public String getCategory()
+        {
+            return category;
+        }
+
+        public Integer getNumberOfPreReqs()
+        {
+            return numberOfPreReqs;
+        }
+
+        public Integer getNumberOfCourseRecommendations()
+        {
+            return numberOfCourseRecommendations;
+        }
+    }
+
+
+    private class NodeWithWeight
     {
         public Node node;
-        public Integer weight;
+        public Double weight;
 
-        public NodeWithGraphWeight(Node node, Integer weight)
+        public NodeWithWeight(Node node, Double weight)
         {
             this.node = node;
             this.weight = weight;
