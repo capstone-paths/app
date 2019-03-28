05a6d8b5ab5baebf0b1cd9deb626149db7bdac46
alexerhardt <alex.erhardt.es@gmail.com>
2019-03-28 19:20:48 +0100

    test cypher alternatives

--- server/routes/learning-paths.js
+++ server/routes/learning-paths.js
@@ -1,4 +1,7 @@
+// TODO: Remove util
+const util = require('util');
 const express = require('express');
+const driver = require('../config/neo4j');
 
 const router = express.Router();
 
@@ -8,8 +11,23 @@ const router = express.Router();
  * @desc   Retrieves a learning path by id
  * @param  id (in-path, mandatory, id)
  */
-router.get('/:id', (req, res) => {
-  res.send('Hello world!');
+router.get('/:id', (req, res, next) => {
+  console.log('GET /api/learning-paths/:id called');
+  // const query = 'MATCH (n)-[seq :NEXT {seqId: 1}]->(m) RETURN n, m, seq';
+  const query = `
+    MATCH path = (c1)-[nextRel: NEXT {seqId: 1}]->(c2)
+    WITH DISTINCT(path) AS path
+    RETURN nodes(path) AS nodes, relationships(path) as rels
+  `;
+
+  const session = driver.session();
+  session
+    .run(query)
+    .then((results) => {
+      res.json(results);
+    })
+    .catch(next)
+    .then(() => session.close())
 });
 
 module.exports = router;
