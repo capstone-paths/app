f6eedb949f048cf5bbfdfd2e7d21424158c71d99
alexerhardt <alex.erhardt.es@gmail.com>
2019-03-28 21:05:58 +0100

    add sequence start to response

--- server/routes/learning-paths.js
+++ server/routes/learning-paths.js
@@ -12,19 +12,30 @@ const router = express.Router();
  * @param  id (in-path, mandatory, id)
  */
 router.get('/:id', (req, res, next) => {
-  console.log('GET /api/learning-paths/:id called');
-  // const query = 'MATCH (n)-[seq :NEXT {seqId: 1}]->(m) RETURN n, m, seq';
   const query = `
-    MATCH path = (c1)-[nextRel: NEXT {seqId: 1}]->(c2)
-    WITH DISTINCT(path) AS path
-    RETURN nodes(path) AS nodes, relationships(path) as rels
+    MATCH (start: SequenceStart {seqId: 1})
+    MATCH ()-[rel :NEXT {seqId: 1}]->(c: Course)
+    RETURN start, collect(DISTINCT c), collect(DISTINCT rel) 
   `;
+
 
   const session = driver.session();
   session
     .run(query)
     .then((results) => {
-      res.json(results);
+      let [nodes, rels] = [[], []];
+
+      // Results contain an array of records, but we only expect one result
+      // Somewhat confusing, but it's how it works; references:
+      // https://neo4j.com/docs/driver-manual/1.7/cypher-values/#driver-result
+      // https://neo4j.com/docs/api/javascript-driver/current/class/src/v1/result.js~Result.html
+      let records = results.records[0];
+
+      let start = records.get("start");
+      let { name, rating } = start.properties;
+      nodes[0] = { nodeId: start.identity.toNumber(), type: "start", name, rating };
+
+      res.json(nodes);
     })
     .catch(next)
     .then(() => session.close())
