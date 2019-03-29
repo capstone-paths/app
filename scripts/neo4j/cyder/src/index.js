aa5ec5ed8939b6fbe78e241a0aeeadfd9944124e
alexerhardt <alex.erhardt.es@gmail.com>
2019-03-29 16:36:32 +0100

    add deletion

--- scripts/neo4j/cyder/src/index.js
+++ scripts/neo4j/cyder/src/index.js
@@ -1,5 +1,6 @@
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '..', '.env') });
+const neo4j = require('neo4j-driver').v1;
 
 const { Command, flags } = require('@oclif/command');
 const { cli } = require('cli-ux');
@@ -7,21 +8,38 @@ const { cli } = require('cli-ux');
 class CyderCommand extends Command {
   async run() {
     const { flags } = this.parse(CyderCommand);
-    const { file, uri, user, pass } = await this.processArgs(flags);
-    this.log(`file: ${file}, uri: ${uri}, user: ${user}, pass: ${pass}`);
+    const { file, uri, user, password, shouldDelete } = await this.processArgs(flags);
+    this.log(`file: ${file}, uri: ${uri}, user: ${user}, pass: ${password}, shouldDelete: ${shouldDelete}`);
+
+    const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
+    const session = driver.session();
+
+    if (shouldDelete) {
+      this.log('Wiping the database...');
+      try {
+        await session.run('MATCH (n) DETACH DELETE n');
+      }
+      catch (e) {
+        session.close();
+        this.error('Deletion failed: ' + e);
+        this.exit(1);
+      }
+    }
+
+    session.close();
   }
 
   async processArgs(flags) {
     const file = flags.file || process.env.DEFAULT_FILE; 
     const uri = flags.uri || process.env.NEO4J_URI;
     const user = flags.user || process.env.NEO4J_USER;
-    const pass = flags.password || process.env.NEO4J_PASSWORD;
+    const password = flags.password || process.env.NEO4J_PASSWORD;
 
     let missing = [];
     if (!file) missing.push('file');
     if (!uri) missing.push('URI');
     if (!user) missing.push('user');
-    if (!pass) missing.push('password');
+    if (!password) missing.push('password');
 
     if (missing.length > 0) {
       this.error('Missing the following parameters: ' + missing.toString());
@@ -37,7 +55,7 @@ class CyderCommand extends Command {
     }
     const shouldDelete = flags.delete;
 
-    return { file, uri, user, pass, shouldDelete };
+    return { file, uri, user, password, shouldDelete };
   }
 }
 
