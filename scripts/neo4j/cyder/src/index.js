3275d3beb391b22c8766ef5b4dc81232e5951cba
alexerhardt <alex.erhardt.es@gmail.com>
2019-03-29 15:57:47 +0100

    add non-interactive flags

--- scripts/neo4j/cyder/src/index.js
+++ scripts/neo4j/cyder/src/index.js
@@ -5,9 +5,30 @@ const { Command, flags } = require('@oclif/command');
 
 class CyderCommand extends Command {
   async run() {
-    const {flags} = this.parse(CyderCommand);
-    const name = flags.name || process.env.HELLO;
-    this.log(`hello ${name} from ./src/index.js`);
+    const { flags } = this.parse(CyderCommand);
+    const { file, uri, user, pass } = this.processArgs(flags);
+    this.log(`file: ${file}, uri: ${uri}, user: ${user}, pass: ${pass}`);
+  }
+
+  processArgs(flags) {
+    const file = flags.file || process.env.DEFAULT_FILE; 
+    const uri = flags.uri || process.env.NEO4J_URI;
+    const user = flags.user || process.env.NEO4J_USER;
+    const pass = flags.password || process.env.NEO4J_PASSWORD;
+
+    let missing = [];
+    if (!file) missing.push('file');
+    if (!uri) missing.push('URI');
+    if (!user) missing.push('user');
+    if (!pass) missing.push('password');
+
+    if (missing.length > 0) {
+      this.error('Missing the following parameters: ' + missing.toString());
+      this.error('Please specify them as flags, or add them to the .env config file');
+      this.exit(1);
+    }
+
+    return { file, uri, user, pass };
   }
 }
 
@@ -21,7 +42,11 @@ CyderCommand.flags = {
   version: flags.version({char: 'v'}),
   // add --help flag to show CLI version
   help: flags.help({char: 'h'}),
-  name: flags.string({char: 'n', description: 'name to print'}),
+  file: flags.string({ char: 'f', description: 'File with Cypher queries to read in'} ),
+  uri: flags.string({ char: 'i', description: 'Neo4j URI' }),
+  user: flags.string({ char: 'u', description: 'Neo4j user' }),
+  password: flags.string({ char: 'p', description: 'Neo4j password' }),
+  delete: flags.boolean({ char: 'd', description: 'Deletes the datbase clean before reading the file in' }),
 }
 
-module.exports = CyderCommand
+module.exports = CyderCommand;
