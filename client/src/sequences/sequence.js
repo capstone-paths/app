bc968c16dc0f72f9102ba4cd3f61fb078b07ff9c
jjkaufman <jonathanjkaufman@gmail.com>
2019-03-30 20:38:52 -0700

    have the sequence component take in sequence id as a prop

--- client/src/sequences/sequence.js
+++ client/src/sequences/sequence.js
@@ -1,75 +1,46 @@
 import vis from 'vis'
 import React, { Component } from 'react';
 import LerntApi from '../LerntApi'
-import './sequence.css';
+import './Sequence.css';
 
 export default class CouseNetworkVis extends Component {
     constructor(props) {
       super(props);
       let api = new LerntApi();
-      api.getSequence(1)
+      api.getSequence(props.sequenceId)
         .then((response) => {
           console.log(response)
           let nodes = response.data.data.courseNodes.map((course) => {
             return {
                 font: { multi: "md", face: "arial", size:20 },
                 color: {background:'white', border:'black'},
-                id: course.id,
+                id: course.nodeId,
                 label: "*" + course.name + "*\n" + course.institution,
               };
           })
-          let edges = response.data.rels
+          let edges = response.data.data.rels.map((rel) =>{
+            return {
+                    from: rel.start,
+                    to: rel.end,
+                    arrows: "to",
+                    color: {
+                      color: "blue"
+                    }
+                  };
+          });
+          console.log(edges)
           this.setState({
               nodes: nodes,
               edges: edges
           })
      });
       this.sequenceId = props.sequenceId;
-      this.state = {
-        items: [
-          {
-            id: 1,
-            text: "Intro to Comp Sci",
-            school: "Coursera: Darmouth",
-            start: "11/01/2019",
-            level: 0
-          },
-          {
-            id: 2,
-            text: "Learn React",
-            school: "Coursera: Stanford",
-            start: "11/01/2019",
-            level: 1
-          },
-          {
-            id: 3,
-            text: "Data Structures",
-            school: "EdX: MIT",
-            start: "11/01/2019",
-            level: 1
-          },
-          {
-            id: 4,
-            text: "Operating Systems",
-            school: "Coursera: Georgia Tech",
-            start: "11/01/2019",
-            level: 2
-          },
-          {
-            id: 5,
-            text: "Computer Vision",
-            school: "Coursera: Georgia Tech",
-            start: "11/01/2019",
-            level: 2
-          },
-       
-        ]
-      };
     }
     componentDidUpdate(prevProps, prevState, snapshot){
         var nodes = new vis.DataSet(this.state.nodes);
-        var edges = new vis.DataSet([]);
+        var edges = new vis.DataSet(this.state.edges);
         var container = document.getElementById("course-sequence");
+        console.log(edges);
         var data = {
           nodes: nodes,
           edges: edges
@@ -77,9 +48,9 @@ export default class CouseNetworkVis extends Component {
         var options = {
           layout: {
             randomSeed: 2,
-            hierarchical: {
-              direction: "LR"
-            }
+            // hierarchical: {
+            //   direction: "LR"
+            // }
           },
     
           nodes: {
