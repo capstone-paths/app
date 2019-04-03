39dc36fa2c0f215ba02e46fc9c6a374940f11ac3
alexerhardt <alex.erhardt.es@gmail.com>
2019-04-03 14:46:14 +0200

    finish reorganizing files

--- scripts/neo4j/read-query/read-query.py
+++ scripts/neo4j/read-query/read-query.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from neo4j import GraphDatabase
+import import_params
 
 filename = sys.argv[1]
 cur = os.path.dirname(os.path.abspath(__file__))
@@ -10,7 +11,7 @@ path = os.path.normpath(joined)
 with open(path, 'r') as file:
     query = file.read()
     driver = GraphDatabase.driver("bolt://localhost:76877", auth=("neo4j", "1234"))
-    with driver.session() as session:
+    with import_params.driver.session() as session:
         session.run(query)
 
 
