7dcfb9366280be57b532e97da5e67ea5a804e270
alexerhardt <alex.erhardt.es@gmail.com>
2019-04-03 15:00:56 +0200

    remove useless line

--- scripts/neo4j/read_query/read_query.py
+++ scripts/neo4j/read_query/read_query.py
@@ -10,7 +10,6 @@ path = os.path.normpath(joined)
 
 with open(path, 'r') as file:
     query = file.read()
-    driver = GraphDatabase.driver("bolt://localhost:76877", auth=("neo4j", "1234"))
     with import_params.driver.session() as session:
         session.run(query)
 
