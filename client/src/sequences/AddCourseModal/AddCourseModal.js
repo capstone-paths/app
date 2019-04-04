06143ffc8fc3a54916ff7f9c32391153082812e2
jjkaufman <jonathanjkaufman@gmail.com>
2019-04-04 07:12:06 -0700

    add Course Details below search

--- client/src/sequences/AddCourseModal/AddCourseModal.js
+++ client/src/sequences/AddCourseModal/AddCourseModal.js
@@ -8,7 +8,7 @@ import LerntApi from '../../LerntApi'
 export default class CouseNetworkVis extends Component {
     constructor(props) {
         super(props);
-        this.state = {loaded: false};
+        this.state = { loaded: false };
         this.api = new LerntApi();
         this.sequenceId = props.sequenceId;
         this.api.getCourses().then((response) => {
@@ -23,14 +23,36 @@ export default class CouseNetworkVis extends Component {
         if (this.state.courses != null) {
             var input = document.getElementById("awesomplete");
             new Awesomplete(input, {
-                list: this.state.courses.map(course => { return { "label": course.name, "value": course } })
+                list: this.state.courses.map(course => { return { "label": course.name, "value": course } }),
+                // insert label instead of value into the input.
+                replace: function (suggestion) {
+                    this.input.value = suggestion.label;
+                }
             });
-            input.addEventListener('awesomplete-selectcomplete', function (e) { console.log(e); }, false);
+            input.addEventListener('awesomplete-selectcomplete', (e) => { 
+                console.log(e);
+                var state = this.state;
+                state.selectedCourse = e.text.value; 
+                this.setState(state);
+            }, false);
         }
     }
     render() {
+        function CourseDetails(props) {
+            const selectedCourse = props.selectedCourse;
+            if (selectedCourse) {
+              return   <div> <div><label>Course Title: </label>{selectedCourse.name}</div>
+              <div><label>Subject: {selectedCourse.subject}</label></div>
+              <div><label>Institution: {selectedCourse.institution}</label></div>
+              </div>;
+            }else{
+                return <div></div>
+            }
+          }
+
         return <div>
             <input id="awesomplete" />
+            <CourseDetails selectedCourse={this.state.selectedCourse} />
         </div>;
     }
 }
