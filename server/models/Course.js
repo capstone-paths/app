4a57609c624f8df5c735536329957e82c7958dc4
jjkaufman <jonathanjkaufman@gmail.com>
2019-04-06 16:09:49 -0700

    retrieve a single course by id

--- server/models/Course.js
+++ server/models/Course.js
@@ -23,6 +23,33 @@ class Course {
       courses: courseData
     };
   }
+    /**
+   * Finds a course by id
+   * @param {Session} session 
+   * @param {Integer} id 
+   */
+  static async findById(session, id) {
+    const query = `
+    MATCH (c: Course {courseID: $id})
+    RETURN c as course`;
+    const results = await session.run(query, { id });
+    if (results.records.length === 0) {
+      return undefined;
+    }
+
+
+    // TODO: Create a generic serialization layer that does this
+    // It should be able to pull it from a schema and do it for any object
+    let courseNodes, rels;
+
+    let records = results.records[0];
+
+    let course = records.get('course');
+    let courseData = course.properties;
+    return {
+      course : courseData
+    };
+  }
 }
 
 module.exports = Course;
