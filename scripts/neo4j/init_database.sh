8749a833c7b5cb9fe1af3bcdccd597ce6a744d87
ajaykliyara <kliyaraphilip@g.harvard.edu>
2019-04-08 17:57:02 -0400

    parameterized db init script to accept python v3 sym link

--- scripts/neo4j/init_database.sh
+++ scripts/neo4j/init_database.sh
@@ -1,17 +1,29 @@
-#!/usr/bin/env python
+#!/bin/sh
 # The script creates a python virual environment , installs all dependencies and runs a python script that interactively
 # allows setting up the db .env file, wipe existing data, load course node and run cypher queries to create sequences
-# This script is tested on a mac and will require Python3 and pip installed
+# This script is tested on a mac and will require Python V3 installed
+# and pip installed
 # installing pip - 1) curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py 2) python get-pip.py
+# ----------------------------------------------------------
+# pass python version 3 symbolic link
+# Example : sh init_database.sh python3.7
+# ----------------------------------------------------------
+
+# check if python3 symlink is passed
+if [ $# -lt 1 ];
+then
+  echo "$0: python v3 symlink not passed [ Example : sh init_database.sh python3.7 ]"
+  exit 2
+fi
 
 # install / upgrade pip
-python -m pip install --upgrade pip
+$1 -m pip install --upgrade pip
 
 # install / upgrade virtual env
-python -m pip install --upgrade virtualenv
+$1 -m pip install --upgrade virtualenv
 
 # set up virtual environment by the name 'env'
-python -m virtualenv env
+$1 -m virtualenv env
 
 # activate env
 source env/bin/activate
@@ -20,7 +32,7 @@ source env/bin/activate
 pip install -r requirements.txt
 
 # execute database init script
-python init_database.py
+$1 init_database.py
 
 # deactivate virtual env
 deactivate
