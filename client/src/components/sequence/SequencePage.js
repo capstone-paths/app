516aa450b1c3c82f71f042a2890d0a4ec9594fa5
alexerhardt <alex.erhardt.es@gmail.com>
2019-04-23 17:15:14 +0200

    refactor functions to be class members

--- client/src/components/sequence/SequencePage.js
+++ client/src/components/sequence/SequencePage.js
@@ -8,98 +8,114 @@ import CourseDetailsMini from '../course/CourseDetailsMini';
 import SubscribeToSequenceButton from './SubscribeToSequenceButton/SubscribeToSequenceButton'
 
 class SequencePage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { loaded: false, currentCourse: null };
-        this.api = new LerntApi();
-        this.api.getSequence(props.match.params.sequenceId)
-            .then((response) => {
-                this.setState({ loaded: true, data: response.data })
-            });
-        this.visRef = React.createRef();
+
+  state = {
+    loaded: true,
+    sequenceData: undefined,
+    currentCourse: undefined,
+    courseRecommendations: undefined,
+  };
+
+  constructor(props) {
+    super(props);
+    this.visRef = React.createRef();
+  }
+
+  componentDidMount() {
+    LerntApi
+      .getSequence(this.props.match.params.sequenceId)
+      .then(response => {
+        this.setState({ loaded: true, sequenceData: response.data })
+      });
+  }
+
+  // Arrow functions allow to access 'this' without binding
+  // https://medium.freecodecamp.org/react-binding-patterns-5-approaches-for-handling-this-92c651b5af56
+  onClick = () => {
+    alert('Add course to sequence');
+  };
+
+  saveSequence = () => {
+    alert('saved');
+  };
+
+  onCourseSelect = (course) => {
+    const { pathId } = this.state.sequenceData;
+    const { selectedCourse } = course.selectedCourse;
+
+    LerntApi
+      .getSequenceCourseRecommendation('2', pathId, selectedCourse)
+      .then(response => {
+        this.setState({ courseRecommendations: response.data });
+      });
+  };
+
+  getCourseDetails = () => {
+    if (this.state.currentCourse) {
+      return (
+        <CourseDetailsMini
+          courseId ={this.state.currentCourse}
+        />
+      )
     }
-   
-    render() {
-        let vis;
 
-        function onclick() {
-            alert('Add course to sequence');
-        }
-        let saveSequence = () => {
-            alert('saved');
-        }
-        let onCourseSelect = (course) => {
-            var state = this.state;
-            state.currentCourse = course.selectedCourse;
-            this.setState(state);
-            this.api.getSequenceCourseRecommendation('2', this.state.data.pathID, course.selectedCourse)
-            .then(response => {
-                var state = this.state;
-                state.courseRecommendations = response.data;
-                this.setState(state);
-            });
-        }
-        let  getCourseDetails = () => {
-            if(this.state.currentCourse !== null){
-                return  <CourseDetailsMini courseId ={this.state.currentCourse} ></CourseDetailsMini>
-            }
-            return ''
-        }
+    return '';
+  };
 
-        if (this.state.loaded) {
-            vis = <CouseNetworkVis ref={this.visRef} sequenceId={this.props.match.params.sequenceId}  onCourseSelect={onCourseSelect}></CouseNetworkVis>
-        } else {
-            //This will block out the page, which sucks. Working on it. 
-            // vis =  <Dimmer active>
-            //             <Loader>Loading</Loader>
-            //         </Dimmer>
-            vis = <div>Loading ... <Icon loading name='spinner' /></div>
-        }
-        return (
-            <div style={{ fontSize: '2em' }}>
-                <Header as='h1' attached='top'>
-                    {this.state.loaded ? this.state.data.sequence.name : ''}
-                <SubscribeToSequenceButton sequenceID ={this.props.match.params.sequenceId}></SubscribeToSequenceButton>
-                <Button color="green" style={{float: 'right'}} onClick={saveSequence}>
-                Save
-                                    <Icon name='right chevron' />
-            </Button>
-                </Header>
-                <Segment attached style={{ padding: "0em" }}>
-                    <Grid celled='internally'>
-                        <Grid.Row>
-                            <Grid.Column width={12}>
-                                {vis}
-                            </Grid.Column>
-                            <Grid.Column width={4} style={{ padding: "0em" }}>
-                                <Menu fluid vertical >
-                                    <Menu.Item>
-                                        <Header as='h4'>Search Courses</Header>
-                                        <AddCourseSearch />
-                                    </Menu.Item>
+  render() {
+    let vis;
 
-                                    <Menu.Item>
-                                        <Header as='h4'>Recommended Courses</Header>
-                                        {this.state.courseRecommendations !== undefined ? this.state.courseRecommendations.map((course) => {
-                                return <Menu.Item name='test' onClick={onclick}>{course.name}</Menu.Item>
-                                        }):''}                                        
-                                    </Menu.Item>
-                                    <Menu.Item>
-                                        <Header as='h4'>Sequence Statistics</Header>
-                                        <p>100% Awesome</p>
-                                    </Menu.Item>
-                                    <Menu.Item>
-                                        {getCourseDetails()}
-                                    </Menu.Item>
-                                </Menu>
-                            </Grid.Column>
-                        </Grid.Row>
-                    </Grid>
-                </Segment>
-
-            </div>
-        );
+    if (this.state.loaded) {
+      vis = <CouseNetworkVis ref={this.visRef} sequenceId={this.props.match.params.sequenceId}  onCourseSelect={this.onCourseSelect}></CouseNetworkVis>
+    } else {
+      vis = <div>Loading ... <Icon loading name='spinner' /></div>
     }
+
+    return (
+      <div style={{ fontSize: '2em' }}>
+        <Header as='h1' attached='top'>
+          {this.state.loaded ? this.state.sequenceData.sequence.name : ''}
+          <SubscribeToSequenceButton sequenceID ={this.props.match.params.sequenceId}></SubscribeToSequenceButton>
+          <Button color="green" style={{float: 'right'}} onClick={this.saveSequence}>
+            Save
+            <Icon name='right chevron' />
+          </Button>
+        </Header>
+        <Segment attached style={{ padding: "0em" }}>
+          <Grid celled='internally'>
+            <Grid.Row>
+              <Grid.Column width={12}>
+                {vis}
+              </Grid.Column>
+              <Grid.Column width={4} style={{ padding: "0em" }}>
+                <Menu fluid vertical >
+                  <Menu.Item>
+                    <Header as='h4'>Search Courses</Header>
+                    <AddCourseSearch />
+                  </Menu.Item>
+
+                  <Menu.Item>
+                    <Header as='h4'>Recommended Courses</Header>
+                    {this.state.courseRecommendations !== undefined ? this.state.courseRecommendations.map((course) => {
+                      return <Menu.Item name='test' onClick={this.onClick}>{course.name}</Menu.Item>
+                    }):''}
+                  </Menu.Item>
+                  <Menu.Item>
+                    <Header as='h4'>Sequence Statistics</Header>
+                    <p>100% Awesome</p>
+                  </Menu.Item>
+                  <Menu.Item>
+                    {this.getCourseDetails()}
+                  </Menu.Item>
+                </Menu>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Segment>
+
+      </div>
+    );
+  }
 }
 
 export default SequencePage;
