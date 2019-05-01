d8bbb9e6f1a79e28ea7bb4d31faa6f29b3bb1813
alexerhardt <alex.erhardt.es@gmail.com>
2019-05-01 20:34:09 +0200

    simplify and fix home

--- client/src/components/home/HomePage.js
+++ client/src/components/home/HomePage.js
@@ -7,47 +7,31 @@ class HomePage extends Component {
 
   render() {
     return (
-      <Grid>
+      <div id='home-container'>
+
         {/* Hero */}
-        <Grid.Row
-          className='hero'
-        >
-          I'll be the hero shot
-          <Grid.Column>
-            <Container
-              className='hero-text'
-              text
-            >
-              <h1>Welcome to Lernt.io</h1>
-              <h2>Class is <em>always</em> in session</h2>
-              <h3>
-                Lernt.io helps you plan your lifelong learning, <br />
-                discover new content to take your skills further, <br />
-                and share your knowledge with others.
-              </h3>
-            </Container>
+        <Grid verticalAlign='middle'>
+          <Grid.Row className='hero'>
+            <Grid.Column>
+              <Container
+                className='hero-text'
+                text
+              >
+                <h1>Welcome to Lernt.io</h1>
+                <h2>Class is <em>always</em> in session</h2>
+                <h3>
+                  Lernt.io helps you plan your lifelong learning, <br />
+                  discover new content to take your skills further, <br />
+                  and share your knowledge with others.
+                </h3>
+              </Container>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
 
-          </Grid.Column>
+      </div>
 
-        </Grid.Row>
 
-        {/* Popular Categories */}
-        <Grid.Row>
-          I'll be the learning path buttons
-
-        </Grid.Row>
-
-        {/* Share Learning Paths */}
-        <Grid.Row>
-          I'll be the contribute
-
-        </Grid.Row>
-
-        {/* Footer */}
-        <Grid.Row>
-          I'll be a footer
-        </Grid.Row>
-      </Grid>
     );
   }
 }
