39264457cdc36062cd0313b262f90464a8c7aac7
alexerhardt <alex.erhardt.es@gmail.com>
2019-05-01 22:44:29 +0200

    finish icon rows

--- client/src/components/home/HomePage.js
+++ client/src/components/home/HomePage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Button, Container, Header, Icon } from 'semantic-ui-react';
+import { Grid, Divider, Card, Button, Container, Header, Icon } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import './Home.css';
 
@@ -28,6 +28,66 @@ class HomePage extends Component {
             </Grid.Column>
           </Grid.Row>
         </Grid>
+
+        <Divider hidden />
+        <Divider hidden />
+
+        <Container text>
+          <Header as='h1'>What do you want to learn?</Header>
+          <Header as='h3'>Browse Learning Paths by Popular Categories</Header>
+          <Grid columns={3}>
+            <Grid.Row>
+              <Grid.Column>
+                <Card centered fluid className='category-card'>
+                  <Icon name='desktop' size='huge' />
+                  <h2>Front-End Development</h2>
+                </Card>
+              </Grid.Column>
+              <Grid.Column>
+                <Card centered fluid className='category-card'>
+                  <Icon name='server' size='huge' />
+                  <h2>Back-End Development</h2>
+                </Card>
+              </Grid.Column>
+              <Grid.Column>
+                <Card centered fluid className='category-card'>
+                  <Icon name='code' size='huge' />
+                  <h2>Full-Stack Development</h2>
+                </Card>
+
+              </Grid.Column>
+
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column>
+                <Card centered fluid className='category-card'>
+                  <Icon name='apple' size='huge' />
+                  <h2>iOS <br /> Development</h2>
+                </Card>
+
+              </Grid.Column>
+              <Grid.Column>
+                <Card centered fluid className='category-card'>
+                  <Icon name='android' size='huge' />
+                  <h2>Android <br /> Development</h2>
+                </Card>
+
+              </Grid.Column>
+              <Grid.Column>
+                <Card centered fluid className='category-card'>
+                  <Icon name='calculator' size='huge' />
+                  <h2>Data <br /> Science</h2>
+                </Card>
+
+              </Grid.Column>
+
+            </Grid.Row>
+
+          </Grid>
+        </Container>
+
+
+
 
       </div>
 
