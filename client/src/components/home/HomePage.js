7dedd77645be4bb1b89b1999f26c35c1da744908
alexerhardt <alex.erhardt.es@gmail.com>
2019-05-02 12:56:17 +0200

    tweak icon rows

--- client/src/components/home/HomePage.js
+++ client/src/components/home/HomePage.js
@@ -9,7 +9,7 @@ class HomePage extends Component {
     return (
       <div id='home-container'>
 
-        {/* Hero */}
+        {/* Section: Hero */}
         <Grid verticalAlign='middle'>
           <Grid.Row className='hero'>
             <Grid.Column>
@@ -18,7 +18,7 @@ class HomePage extends Component {
                 text
               >
                 <h1>Welcome to Lernt.io</h1>
-                <h2>Class is <em>always</em> in session</h2>
+                <h3>Class is <em>always</em> in session</h3>
                 <h3>
                   Lernt.io helps you plan your lifelong learning, <br />
                   discover new content to take your skills further, <br />
@@ -32,61 +32,64 @@ class HomePage extends Component {
         <Divider hidden />
         <Divider hidden />
 
+        {/* Section: Categories */}
         <Container text>
           <Header as='h1'>What do you want to learn?</Header>
           <Header as='h3'>Browse Learning Paths by Popular Categories</Header>
           <Grid columns={3}>
+
             <Grid.Row>
               <Grid.Column>
-                <Card centered fluid className='category-card'>
-                  <Icon name='desktop' size='huge' />
-                  <h2>Front-End Development</h2>
+                <Card raised centered fluid className='category-card'>
+                  <Icon name='react' className='category-card__icon' />
+                  <h3>Front-End <br /> Development</h3>
                 </Card>
               </Grid.Column>
               <Grid.Column>
-                <Card centered fluid className='category-card'>
-                  <Icon name='server' size='huge' />
-                  <h2>Back-End Development</h2>
+                <Card raised centered fluid className='category-card'>
+                  <Icon name='node js' className='category-card__icon' />
+                  <h3>Back-End <br /> Development</h3>
                 </Card>
               </Grid.Column>
               <Grid.Column>
-                <Card centered fluid className='category-card'>
-                  <Icon name='code' size='huge' />
-                  <h2>Full-Stack Development</h2>
+                <Card raised centered fluid className='category-card'>
+                  <Icon name='stack overflow' className='category-card__icon' />
+                  <h3>Full-Stack <br /> Development</h3>
                 </Card>
-
               </Grid.Column>
-
             </Grid.Row>
+
             <Grid.Row>
               <Grid.Column>
-                <Card centered fluid className='category-card'>
-                  <Icon name='apple' size='huge' />
-                  <h2>iOS <br /> Development</h2>
+                <Card raised centered fluid className='category-card'>
+                  <Icon name='apple' className='category-card__icon'/>
+                  <h3>iOS <br /> Development</h3>
                 </Card>
-
               </Grid.Column>
               <Grid.Column>
-                <Card centered fluid className='category-card'>
-                  <Icon name='android' size='huge' />
-                  <h2>Android <br /> Development</h2>
+                <Card raised centered fluid className='category-card'>
+                  <Icon name='android' className='category-card__icon ' />
+                  <h3>Android <br /> Development</h3>
                 </Card>
-
               </Grid.Column>
               <Grid.Column>
-                <Card centered fluid className='category-card'>
-                  <Icon name='calculator' size='huge' />
-                  <h2>Data <br /> Science</h2>
+                <Card raised centered fluid className='category-card'>
+                  <Icon name='python' className='category-card__icon' />
+                  <h3>Data <br /> Science</h3>
                 </Card>
-
               </Grid.Column>
-
             </Grid.Row>
 
           </Grid>
         </Container>
 
+        <Divider hidden />
+        <Divider hidden />
 
+        {/* Section: Share Your Knowledge */}
+        <Container text>
+
+        </Container>
 
 
       </div>
