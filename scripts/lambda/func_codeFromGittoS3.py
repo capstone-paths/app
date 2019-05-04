ec35f30ad5558577097fb07211b19f3941450fb5
ajaykliyara <kliyaraphilip@g.harvard.edu>
2019-05-04 12:46:20 -0400

    Lambda Function : Success/Failure message back to code pipeline

--- scripts/lambda/func_codeFromGittoS3.py
+++ scripts/lambda/func_codeFromGittoS3.py
@@ -27,10 +27,11 @@ def lambda_handler(event, context):
     codepipeline = boto3.client('codepipeline')
     job_id = event["CodePipeline.job"]["id"]
 
-    if (s3.meta.client.copy(code_zip_file, dest_bucket_name, dest_file_name) ):
+    try:
+        s3.meta.client.copy(code_zip_file, dest_bucket_name, dest_file_name
         codepipeline.put_job_success_result(jobId = job_id)
-    else:
-        codepipeline.put_job_failure_result(jobId = job_id, failureDetails = {'message': "zip move to s3 failed", 'type': 'JobFailed'})
+    except e:
+        codepipeline.put_job_failure_result(jobId = job_id, failureDetails = {'message': e, 'type': 'JobFailed'})
 
 
     message = "File moved to S3 Bucket : " + dest_bucket_name + "/" + dest_file_name
