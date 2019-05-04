d1c441ee321e82b51430b8afd29727676f8a1bb5
alexerhardt <alex.erhardt.es@gmail.com>
2019-05-04 18:04:15 +0200

    add libraries and write initial mock data

---
+++ server/tests/mocks/index.js
@@ -0,0 +1,14 @@
+module.exports.setInitState = `
+  CREATE (t001 :Track :TestNode { trackID: 'ST_Track1', name: 'ST_Track1' }) 
+  CREATE (c001 :Course :TestNode { courseID: 'ST_Course1', name: 'ST_Course1' })
+  CREATE (p001 :PathStart :TestNode { pathID: 'ST_Path1', name: 'ST_Path1' })
+  
+  CREATE 
+    (p001)-[:NEXT]->(c001),
+    (c001)-[:NEXT]->(t001)
+`;
+
+module.exports.wipeMockData = `
+  MATCH (n: TestEntity)
+  DETACH DELETE n
+`;
